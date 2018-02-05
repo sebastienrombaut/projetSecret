6f8f768d53ccf5d72310b4b5efe4173ec74e03be
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-02-05 05:35:06 +0100

    crud fait

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/show'
+
+  resources :users
+  root 'users#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
