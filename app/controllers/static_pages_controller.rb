9cb667267b1685867b37027a7f4cf931c24871e8
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-02-05 14:11:16 +0100

    fin partie 5

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,7 @@ class StaticPagesController < ApplicationController
 
   def secret
   	unless logged_in?
+		flash[:success] = "Ca c'est secret ! Pense à te créer un compte pour y avoir accès 😉"
   		redirect_to new_user_path
   	end
   end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Member
   class Application < Rails::Application
+  	config.assets.precompile << "audios/*"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
